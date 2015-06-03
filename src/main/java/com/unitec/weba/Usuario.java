d40db3c1f712955d965b4bdf98745e134f96fd26
T-107 <T-107@PC245>
2015-06-02 21:47:34 -0500

    Hibernate y Jpa

--- src/main/java/com/unitec/weba/Usuario.java
+++ src/main/java/com/unitec/weba/Usuario.java
@@ -5,40 +5,57 @@
  */
 package com.unitec.weba;
 
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
-public class Usuario {
-    private Integer id;
+/**
+ *
+ * @author T-107
+ */
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    
+    @Column(name = "ID_USUARIO")
+    private Integer idUsuario;
+    
+    @Column(name = "NOMBRE")
     private String nombre;
+    
+    @Column(name = "EMAIL")
     private String email;
+    
+    @Column(name = "LOGIN")
     private String login;
+    
+    @Column(name = "PASSWORD")
     private String password;
-
-    @Override
-    public String toString() {
-        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", email=" + email + ", login=" + login + ", password=" + password + '}';
-    }
-
+//CONSTRUCTOR DEL USUARIO
     public Usuario() {
     }
-
-    public Usuario(Integer id) {
-        this.id = id;
+//CONSTRUCTOR PARA BUSCAR POR ID
+    public Usuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
     }
 
-    public Usuario(Integer id, String nombre, String email, String login, String password) {
-        this.id = id;
-        this.nombre = nombre;
-        this.email = email;
-        this.login = login;
-        this.password = password;
+    public Integer getIdUsuario() {
+        return idUsuario;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
     }
 
     public String getNombre() {
@@ -72,6 +89,30 @@ public class Usuario {
     public void setPassword(String password) {
         this.password = password;
     }
-            
-           
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.unitec.weba.Usuario[ idUsuario=" + idUsuario + " ]";
+    }
+    
 }
